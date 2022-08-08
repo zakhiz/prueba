f00283e52b51952cc11b30440f23ce3eadec6ae2
jimmy sebastian higa ramirez <sebascursoshtml@gmail.com>
2022-08-08 19:50:24 -0300

    pruebaDePush

---
+++ js/app.js
@@ -0,0 +1,50 @@
+//const desayuno = "cafe";
+//(desayuno == "cafe") ? "Desayuno cafe con leche" : (desayuno == te) ? "Desayuno te con leche" : (desayuno == "chocolatada") ? "desayuno chocolatada": "desayuno mate";
+
+/*console.log(undefined ?? "hola"); 
+
+const  trabajador1 = {
+    username : "maria",
+    password : 1234,
+    email : "p@p.com",
+    sueldo : 4500
+}
+
+const  trabajador2 = {
+    username : "pepe",
+    password : 1234,
+    email : "p@p.com",
+    sueldo : 4000
+}
+
+const  trabajador3 = {
+    username : "pedro",
+    password : 1234,
+    email : "p@p.com",
+    sueldo : undefined
+}
+
+const  trabajador4 = {
+    username : "rocio",
+    password : 1234,
+    email : "p@p.com",
+    sueldo : 5000
+}
+
+const trabajadores = [trabajador1, trabajador2, trabajador3, trabajador4];
+
+let acumulador = 0;
+
+trabajadores.forEach(trabajador =>{
+    acumulador += trabajador.sueldo ?? 0;
+})
+
+const boton1 = document.getElementById("boton1");
+
+boton1.addEventListener('dblclick',() =>{ //* click es cuando le das un solo click y te ejecuta el evento con doble click tenes que dar doble click para ejecutarlo
+    console.log("Diste click en add event");
+})
+
+//! jsx js vanilla y react
+//* eventos que mas se usan: evento click , doble click , input , change, submit
+*/
